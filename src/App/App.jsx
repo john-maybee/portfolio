5826eecef228037cbd6ad0a9284176891855f628
John Maybee <john.d.maybee@gmail.com>
2023-04-02 17:35:48 -0400

    Ran npm install. Also Added an App folder that holds both the app.jsx and the app.css. The App.css is pretty simple right now, and the App.jsx provides routes to the different components that I will be adding and displays them within a div inside the router.

---
+++ src/App/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  HashRouter as Router,
+  Redirect,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import'./App.css';
+
+// could add a header that I import here then put before the nav in the return
+import Nav from '../Nav/Nav';
+import Footer from '../Footer/Footer';
+import AboutPage from '../AboutPage/AboutPage';
+import HomePage from '../HomePage/HomePage';
+
+
+import './App.css';
+
+
+///////////////////////////////////App function///////////////////////////////////////
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
+          {/* Visiting localhost:3000 will redirect to localhost:3000/home */}
+          <Redirect exact from="/" to="/home" />
+
+          {/* Visiting localhost:3000/home will show the home page. */}
+          <Route exact path="/home">
+            <HomePage />
+          </Route>
+
+          {/* Visiting localhost:3000/about will show the about page. */}
+          <Route
+            exact
+            path="/about"
+          >
+            <AboutPage />
+          </Route>
+
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
+  );
+} // End of the App Function that contains all of the 
+// routes utilized and displays all of them onto the dom. 
+
+export default App;
