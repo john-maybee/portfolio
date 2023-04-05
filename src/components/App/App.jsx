9ec003b2db505f9dbf7ae1f86d4c6d6f37d9aa1f
John Maybee <john.d.maybee@gmail.com>
2023-04-05 13:46:44 -0400

    Updated the NAV bar to incorporate icons that link to the different pages. Gave each of the icons a Tooltip that explains what page the icon will route them to.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -12,6 +12,10 @@ import Nav from '../Nav/Nav';
 import Footer from '../Footer/Footer';
 import AboutPage from '../AboutPage/AboutPage';
 import HomePage from '../HomePage/HomePage';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 
 import './App.css';
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Work } from "@mui/icons-material";
+import { Work, Cottage } from "@mui/icons-material";
+import { Tooltip, Divider } from "@mui/material";
+
 
 function Nav() {
     return(
         <div className="navContainer">
-            <p>NAAAAVVVVV</p>
-            <Link className="navLink" to="/about" >
-                <Work sx={{color: '#223651'}}/>
-            </Link>
+            <Tooltip title="Home" arrow>
+                <Link className="navLink" to="/home" >
+                    <Cottage sx={{color: '#223651'}}/>
+                </Link>
+            </Tooltip>
+            {/* <Divider orientation="vertical" variant="middle" flexItem /> */}
+            <h1>jm</h1>
+            {/* <Divider orientation="vertical" variant="middle" flexItem /> */}
+            <Tooltip title="Resumé" arrow>
+                <Link className="navLink" to="/about" >
+                    <Work sx={{color: '#223651'}}/>
+                </Link>
+            </Tooltip>
         </div>
     )
 }
