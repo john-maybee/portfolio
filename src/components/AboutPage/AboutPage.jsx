ac31b17d25bf4de01db5dc58a266eaa8b54b29bb
John Maybee <john.d.maybee@gmail.com>
2023-04-07 14:19:00 -0400

    Added both pages into a div within the AboutPage.jsx file. Will add styling to this page next, so the images are not larger than the display.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -7,6 +7,14 @@ function AboutPage() {
     <div className="container">
       <div className="headerContainer">
           <h2>My Resumé</h2>
+          <div className="resumeContainer">
+            <div className="firstPageContainer">
+                <img className='firstPageImg' src={firstpage} alt='image of page 1 of my resumé' />
+            </div>
+            <div className="secondPageContainer">
+                <img className='secondPageImg' src={secondpage} alt='image of page 2 of my resumé' />
+            </div>
+          </div>
       </div>
     </div>
   );
