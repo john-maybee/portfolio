bfe029491d15c3ed96aac72d7b6279eba35165fd
John Maybee <john.d.maybee@gmail.com>
2023-04-10 12:06:04 -0400

    Updated the theme on for the footer to the same as the rest of the website.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const theme = createTheme({
+    typography: {
+      fontFamily: [
+        'Roboto',
+      ],
+    }
+});
 
 function Footer() {
     return(
-        <div className='footer'>
-            <p>&copy; John Maybee 2023</p>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className='footer'>
+                <Typography variant='body2' fontWeight={300}>&copy; John Maybee 2023</Typography>
+            </div>
+        </ThemeProvider>
     )
 }
 
