f3709fdaa1c99c2b95e715b92dfac9fbf423b0f1
John Maybee <john.d.maybee@gmail.com>
2023-04-12 13:22:03 -0400

    Created an Image List that holds all of the images from my full stack solo project. Made this list scrollable and made the container large enough to hold more than one list when wrapped.

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -9,6 +9,9 @@ import createartist from './tatcreateartist.png';
 import artists from './tatartistspage.png';
 import about from './tataboutpage.png';
 import tatregister from './tatregister.png';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
 import { Email, GitHub, LinkedIn, Work } from '@mui/icons-material';
 import { Tooltip } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
@@ -70,6 +73,66 @@ function HomePage() {
                                 </Tooltip>
                         </Typography>
                     </div>
+                </div>
+                <div className='imageListContainer'>
+                    <ImageList sx={{width: 400, height: 900}} cols={1}>
+                        <ImageListItem>
+                            <img src={tatuser} alt='Image of the user home page from To Tat solo project' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='User Home Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={createidea} alt='Image of the create new idea page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='Create Idea Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={ideas} alt='Image of the user ideas page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='Ideas Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={editidea} alt='Image of the edit idea page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='Edit Idea Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={createartist} alt='Image of the create artist page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='Create Artist Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={artists} alt='Image of the user artists page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='Artists Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={about} alt='Image of the about To Tat page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='About Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                        <ImageListItem>
+                            <img src={tatregister} alt='Image of the registration page' loading='lazy'/>
+                            <ImageListItemBar 
+                                title='Register Page'
+                                position='below'
+                            />
+                        </ImageListItem>
+                    </ImageList>
                 </div>
             </div>
         </ThemeProvider>
